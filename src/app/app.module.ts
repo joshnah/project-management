81008b60c1096fb95b35ef509cafe4f888aa32e3
joshnah <tiendatamser296@gmail.com>
2023-10-02 15:31:49 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TodoListComponent } from './homepage/todo-list/todo-list.component';
+import { TodoItemComponent } from './homepage/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomepageComponent,
+    TodoListComponent,
+    TodoItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
